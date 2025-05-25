16227892cee802a055c29f0bf83ee492d6f06585
c0a2420223 <c0a2420223@edu.teu.ac.jp>
2025-05-26 01:48:30 +0900

    りんご

--- block.py
+++ block.py
@@ -11,6 +11,8 @@ HEIGHT = 650   # 画面の高さ
 FPS = 60       # フレームレート（1秒あたりの更新回数）
 PADDLE_Y_OFFSET = 50  # 下からバーを上げる量
 
+
+
 # 作業ディレクトリをスクリプトのある場所に変更
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -29,6 +31,7 @@ class Background:
 
     def draw(self, screen):
         screen.blit(self.img, (0, 0))
+        
 
 class Paddle:
     def __init__(self, pos):
@@ -63,6 +66,9 @@ class Paddle:
         char_rect = char.get_rect(midtop=(mx, my - 5))  # 5px 上にオフセット
         screen.blit(char, char_rect)
 
+
+        
+
 class Ball:
     def __init__(self, pos, radius=10):
         self.pos = pg.math.Vector2(pos)
@@ -81,6 +87,7 @@ class Ball:
             self.vel.x *= -1
         if self.pos.y - self.radius <= 0:
             self.vel.y *= -1
+        
 
     def draw(self, screen):
         pg.draw.circle(screen, self.color, (int(self.pos.x), int(self.pos.y)), self.radius)
@@ -90,6 +97,35 @@ class Ball:
             int(self.pos.x - self.radius),
             int(self.pos.y - self.radius),
             self.radius * 2, self.radius * 2
+        )
+
+class Ringo:  #りんごのクラス
+    def __init__(self,ap_x,ap_y,ap_rad):
+        self.ap_x = ap_x
+        self.ap_y = ap_y
+        self.color = (255,0,0)
+        self.ap_rad = ap_rad
+        #座標と半径、色
+        
+    def update(self):
+        
+        self.ap_y += 2
+        
+            
+    def draw(self,screen):
+        
+        # pg.draw.circle(screen,self.color,(self.ap_x, int(self.ap_y)),self.ap_rad)
+        app_img = pg.image.load("fig/apple.png")
+        app_img = pg.transform.scale(app_img,(60,60))
+        screen.blit(app_img,(self.ap_x, int(self.ap_y)))
+        
+            
+
+    def get_rect(self) -> pg.Rect:
+        return pg.Rect(
+            int(self.ap_x - self.ap_rad),
+            int(self.ap_y - self.ap_rad),
+            self.ap_rad * 2, self.ap_rad * 2
         )
 
 class HUD:
@@ -109,12 +145,11 @@ class Game:
         pg.display.set_caption("ブロック崩し")
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
-
         self.bg = Background()
         # バーとキャラをより上に配置
         self.paddle = Paddle((WIDTH//2, HEIGHT - PADDLE_Y_OFFSET))
         self.ball = Ball((self.paddle.rect.centerx, self.paddle.rect.top - 10))
-
+        self.app = Ringo(100,-500,30)
         self.font = pg.font.Font(None, 36)
         self.hud = HUD(self.font)
         self.running = True
@@ -122,15 +157,20 @@ class Game:
 
     def run(self):
         while self.running:
+
             self.clock.tick(FPS)
             self._events()
             self._update()
             self._draw()
+            
+
             if self.hud.hp <= 0:
                 self._draw_game_over()
                 pg.display.flip()
                 pg.time.delay(3000)
                 self.running = False
+            
+
         pg.quit()
         sys.exit()
 
@@ -143,14 +183,30 @@ class Game:
         keys = pg.key.get_pressed()
         self.paddle.update(keys)
         self.ball.update()
+        self.app.update()
 
         if self.ball.get_rect().colliderect(self.paddle.rect):
             self.ball.vel.y *= -1
+        
+        if self.app.get_rect().colliderect(self.paddle.rect):
+            if self.hud.hp < 3 :
+                self.hud.hp += 1
+            else :
+                None
+            self.app.ap_x = random.randint(100,2300)
+            self.app.ap_y = -200
+        
 
         if self.ball.pos.y - self.ball.radius > HEIGHT:
             self.hud.hp -= 1
             pg.time.delay(500)
             self.ball = Ball((self.paddle.rect.centerx, self.paddle.rect.top - 10))
+            self.app.ap_y = -200
+            self.app.ap_x = random.randint(100,2300)
+            #リンゴが落ちたときに再度落ちてくる
+        if self.app.ap_y >= 700:
+            self.app.ap_y = -200
+            self.app.ap_x = random.randint(100,2300)
 
     def _draw(self):
         self.screen.fill((0, 0, 0))
@@ -158,7 +214,9 @@ class Game:
         self.paddle.draw(self.screen)
         self.ball.draw(self.screen)
         self.hud.draw(self.screen)
+        self.app.draw(self.screen)
         pg.display.flip()
+        
 
     def _draw_game_over(self):
         overlay = pg.Surface((WIDTH, HEIGHT))
@@ -168,6 +226,7 @@ class Game:
         game_over_surf = self.game_over_font.render("Game Over", True, (255, 0, 0))
         rect = game_over_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2))
         self.screen.blit(game_over_surf, rect)
+        
 
 
 def main():
